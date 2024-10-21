1e188b359c848595d0c13163a5edf434765d0f59
kaliganoff <kaliganoff@yahoo.com>
2024-10-21 22:41:52 +0300

    feat: Implement PUT and DELETE

--- index.ts
+++ index.ts
@@ -34,8 +34,43 @@ createServer((req, resp) => {
                     resp.end('The request body does not contain required fields');
                 }
         });
+    } else if (req.url?.startsWith('/api/users/') && req.method === 'PUT' && req.url.length > '/api/users/'.length) {
+        let body: any = [];
+        req
+        .on('data', chunk => {
+            body.push(chunk);
+        })
+        .on('end', () => {
+            body = Buffer.concat(body).toString();
+                const { username, age, hobbies } = JSON.parse(body);
+                if (username && age && hobbies) {
+                    const index = users.findIndex((item: any) => item.id === req.url?.replace('/api/users/', ''))
+                    if (users[index]) {
+                        users[index] = { id: users[index].id, username, age, hobbies}
+                        resp.writeHead(200, { 'Content-Type': 'application/json'});
+                        resp.end(JSON.stringify(users[index]));
+                    } else {
+                        resp.writeHead(404, { 'Content-Type': 'text/plain'});
+                        resp.end('User not found');
+                    }
+                } else {
+                    resp.writeHead(400, { 'Content-Type': 'text/plain'});
+                    resp.end('The request body does not contain required fields');
+                }
+        });
+    } else if (req.url?.startsWith('/api/users/') && req.method === 'DELETE' && req.url.length > '/api/users/'.length) {
+        const index = users.findIndex((item: any) => item.id === req.url?.replace('/api/users/', ''))
+        if (users[index]) {
+        users.splice(index, 1);
+        resp.writeHead(204, { 'Content-Type': 'text/plain'});
+        resp.end("User deleted");
+        } else {
+            resp.writeHead(404, { 'Content-Type': 'text/plain'});
+            resp.end('User not found');
+        }
     }
     else {
+        resp.writeHead(404, { 'Content-Type': 'text/plain'});
         resp.write('Page not found')
         resp.end();
     }
